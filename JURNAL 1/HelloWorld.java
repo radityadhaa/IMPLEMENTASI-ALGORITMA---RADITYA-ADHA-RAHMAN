b96b05abb83f12727a2214a344d7cde193f610ec
RADITYA ADHA RAHMAN <radityadhaa@student.telkomuniversity.ac.id>
2021-09-25 10:48:29 +0700

    HelloWorld.java

---
+++ JURNAL 1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args)  {
+		System.out.println("Nama: Raditya Adha Rahman");
+		System.out.println("NIM: 6706213004");
+		System.out.println("Kelas: 45-03");
+	}
+}
